3bc10383bf567c799c03201495bf7b8a4b3a7c2b
Loan Anh <loananh30101995@gmail.com>
2017-11-20 17:09:28 +0700

    bai tap Design Pattern

--- Design-Pattern/chart2/JS/javascript.js
+++ Design-Pattern/chart2/JS/javascript.js
@@ -10,13 +10,13 @@ var options = {
 	var Chart = (function() {
     // Private
     var canvas = options.canvas;
-    var graph = canvas.getContext("2d");
+    var ctx = canvas.getContext("2d");
     canvas.height = 1100;
     canvas.width = 1100;
     
     /**
      * Funtion to draw Arc shape
-     * @param {canvas} graph
+     * @param {canvas} ctx
      * @param {number} point startX
      * @param {number} point startY
      * @param {number} point stopX
@@ -24,60 +24,60 @@ var options = {
      * @param {number} width of line
      * @param {color} color of line
      */
-    drawLine = function(graph,startX, startY, stopX, stopY, width, color) {
-    graph.beginPath();
-    graph.moveTo(startX, startY);
-    graph.lineTo(stopX, stopY);
-    graph.lineWidth= width;
-    graph.strokeStyle = color;
-    graph.stroke();
+    drawLine = function(ctx,startX, startY, stopX, stopY, width, color) {
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(stopX, stopY);
+    ctx.lineWidth= width;
+    ctx.strokeStyle = color;
+    ctx.stroke();
     } 
     /**
      * Funtion to draw Arc shape
-     * @param {canvas} graph
+     * @param {canvas} ctx
      * @param {pixel font} font text
      * @param {number} point startX
      * @param {number} point startY
      * @param {color} color of text
      */
-    drawText = function(graph,font, text, startX, startY, color) {
-      graph.font= font;
-      graph.fillStyle= color;
-      graph.fillText(text,startX,startY);
+    drawText = function(ctx,font, text, startX, startY, color) {
+      ctx.font= font;
+      ctx.fillStyle= color;
+      ctx.fillText(text,startX,startY);
     }
     /**
      * Function to draw chart2
      */
     function drawChart2() {
-        graph.beginPath();
-        graph.moveTo(90, 300);
-        graph.quadraticCurveTo(100, 310, 150, 160)
-        graph.bezierCurveTo(220, -10, 180, 550, 280, 150);
-        graph.quadraticCurveTo(300, 90, 330, 180)
-        graph.quadraticCurveTo(350, 200, 400, 170)
-        graph.quadraticCurveTo(420, 155, 440, 150)
-        graph.lineTo(450, 150);
-        graph.lineJoin = 'round';
-        graph.lineWidth = 4;
-        graph.strokeStyle = '#00aeef';
-        graph.stroke();
+        ctx.beginPath();
+        ctx.moveTo(90, 300);
+        ctx.quadraticCurveTo(100, 310, 150, 160)
+        ctx.bezierCurveTo(220, -10, 180, 550, 280, 150);
+        ctx.quadraticCurveTo(300, 90, 330, 180)
+        ctx.quadraticCurveTo(350, 200, 400, 170)
+        ctx.quadraticCurveTo(420, 155, 440, 150)
+        ctx.lineTo(450, 150);
+        ctx.lineJoin = 'round';
+        ctx.lineWidth = 4;
+        ctx.strokeStyle = '#00aeef';
+        ctx.stroke();
         }
     /**
      * Function to draw coordinate axis in chart2
      */
     function drawLineChart2() {
-        drawLine(graph,60,450,500,450,2,"black");
-        drawLine(graph,60,450,60,20,2,"black");
+        drawLine(ctx,60,450,500,450,2,"black");
+        drawLine(ctx,60,450,60,20,2,"black");
     }
     /**
      * Function to draw text in chart2
      */
     function drawTextChart2() {
-        drawText(graph,"40px Arial", 0, 30, 450, "black");
-        drawText(graph,"40px Arial", 1, 30, 360, "black");
-        drawText(graph,"40px Arial", 2, 30, 270, "black");
-        drawText(graph,"40px Arial", 3, 30, 180, "black");
-        drawText(graph,"40px Arial", 4, 30, 90, "black");
+        drawText(ctx,"40px Arial", 0, 30, 450, "black");
+        drawText(ctx,"40px Arial", 1, 30, 360, "black");
+        drawText(ctx,"40px Arial", 2, 30, 270, "black");
+        drawText(ctx,"40px Arial", 3, 30, 180, "black");
+        drawText(ctx,"40px Arial", 4, 30, 90, "black");
     }
     //Return value for function draw
     return {
